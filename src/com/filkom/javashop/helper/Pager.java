cb849dc12b2cbee15782bc7c56466970ecaef547
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-25 15:17:30 +0700

    Add set pager

--- src/com/filkom/javashop/helper/Pager.java
+++ src/com/filkom/javashop/helper/Pager.java
@@ -9,6 +9,14 @@ public class Pager {
         this.pageWidth = pageWidth;
     }
 
+    public void setHorizontalBar(char bar) {
+        this.horizontalBar = bar;
+    }
+
+    public void setVerticalBar(char bar) {
+        this.verticalBar = bar;
+    }
+
     private void horizontalSeparator() {
         for (int i = 0; i < this.pageWidth; i++) {
             System.out.print(horizontalBar);
@@ -18,6 +26,7 @@ public class Pager {
 
     public void messageCenter(String message) {
         int half = this.pageWidth / 2;
+        System.out.print(this.verticalBar);
         for (int i = 0; i < half; i++) {
             System.out.print(" ");
         }
@@ -25,5 +34,7 @@ public class Pager {
         for (int i = 0; i < half; i++) {
             System.out.print(" ");
         }
+        System.out.print(this.verticalBar);
+        System.out.println();
     }
 }
