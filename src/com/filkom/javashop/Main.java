bfb3ca8e8fbc668eba4714635da9d639f6be524e
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-25 14:57:51 +0700

    Initialize repository

---
+++ src/com/filkom/javashop/Main.java
@@ -0,0 +1,10 @@
+package com.filkom.javashop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+}
