e7eb920517accf7c9c30037811426045cca619ed
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-25 15:55:38 +0700

    Fix pager

--- src/com/filkom/javashop/helper/Pager.java
+++ src/com/filkom/javashop/helper/Pager.java
@@ -4,6 +4,7 @@ public class Pager {
     private int pageWidth = 25;
     private char horizontalBar = '=';
     private char verticalBar = '|';
+    private char edge = '+';
 
     public void setPageWidth(int pageWidth) {
         this.pageWidth = pageWidth;
@@ -17,38 +18,49 @@ public class Pager {
         this.verticalBar = bar;
     }
 
+    public void setEdge(char edge) {
+        this.edge = edge;
+    }
+
     private void horizontalSeparator() {
+        System.out.print(this.edge);
         for (int i = 0; i < this.pageWidth; i++) {
             System.out.print(horizontalBar);
         }
+        System.out.print(this.edge);
         System.out.println();
     }
 
     private void spacer(int space) {
         for (int i = 0; i < space; i++) {
-            System.out.print(horizontalBar);
+            System.out.print(verticalBar);
             for (int j = 0; j < pageWidth; j++) {
                 System.out.print(" ");
             }
-            System.out.print(horizontalBar);
+            System.out.print(verticalBar);
             System.out.println();
         }
     }
 
     public void messageCenter(String message) {
         horizontalSeparator();
-        spacer(2);
-        int half = this.pageWidth / 2;
+        spacer(1);
+        int size = pageWidth - 2;
         System.out.print(this.verticalBar);
-        for (int i = 0; i < half; i++) {
+        for (int i = 1; i < size / 2; i++) {
             System.out.print(" ");
         }
         System.out.print(message);
-        for (int i = 0; i < half; i++) {
+        size -= message.length();
+        for (int i = -1; i < size / 2; i++) {
+            System.out.print(" ");
+        }
+        if (pageWidth % 2 != 0) {
             System.out.print(" ");
         }
         System.out.print(this.verticalBar);
         System.out.println();
-        spacer(2);
+        spacer(1);
+        horizontalSeparator();
     }
 }
