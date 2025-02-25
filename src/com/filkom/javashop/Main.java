396911c1157ea3a0370101cf215ad0bc43c2a327
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-25 16:18:38 +0700

    Initialize welcome messsage

--- src/com/filkom/javashop/Main.java
+++ src/com/filkom/javashop/Main.java
@@ -1,10 +1,22 @@
-package com.filkom.javashop;
-
 import java.util.Scanner;
+import helper.Pager;
 
 public class Main {
+
+    static Pager pager = new Pager();
+
+    private static void welcomeMessage() {
+        pager.horizontalSeparator();
+        pager.spacer();
+        pager.messageCenter("Welcome to javaShop");
+        pager.messageCenter("Available products");
+        pager.spacer();
+        pager.horizontalSeparator();
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        welcomeMessage();
         input.close();
     }
 }
