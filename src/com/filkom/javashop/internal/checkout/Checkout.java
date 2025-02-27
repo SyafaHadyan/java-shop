801809c46b3f795061118bfff945463f3be43cf4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-27 16:39:45 +0700

    Delete files

--- src/com/filkom/javashop/internal/checkout/Checkout.java
+++ src/com/filkom/javashop/internal/checkout/Checkout.java
@@ -2,6 +2,7 @@
 package com.filkom.javashop.internal.checkout;
 
 import com.filkom.javashop.internal.item.Item;
+import com.filkom.javashop.internal.checkout.Checkout;
 import java.util.ArrayList;
 import java.util.List;
 
